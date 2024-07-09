ad9b2e18908582b47298010fba6cbff0649972f4
ASWINCODEnERROR <8102018aswin@gmail.com>
2024-07-09 18:12:19 +0530

    13 successfully

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,17 +12,19 @@ urlpatterns = [
 #    post endpoints
     path('post/category/list/',api_views.CategoryListAPIView.as_view()),
     path('post/category/posts/<category_slug>/',api_views.CategoryListAPIView.as_view()),
-    path('post/list/',api_views.PostListAPIView.as_view()),
+    path('post/lists/',api_views.PostListAPIView.as_view()),
     path('post/detalil/<slug>/',api_views.PostDetailAPIView.as_view()),
     path('post/like-post/',api_views.LikePostAPIView.as_view()),
     path('post/comment-post/',api_views.PostCommentAPIView.as_view()),
     path('post/bookmark-post/',api_views.BookmarkPostAPIView.as_view()),
+    
+    
     path('author/dashboard/stats/<user_id>',api_views.DashboardStats.as_view()),
-    # path('author/dashboard/stats/<int:user_id>/',api_views.DashboardStats.as_view()),
+    path('author/dashboard/comment-list/<user_id>',api_views.DashboardCommentList.as_view()),
+    path('author/dashboard/noti-list/<user_id>',api_views.DashboardNotificatonsList.as_view()),
+    path('author/dashboard/noti-mark-seen/',api_views.DashboardMarkNotificationAsSeen.as_view()),
+    path('author/dashboard/reply-comment/',api_views.DashboardReplyCommentAPIView.as_view()),
     
 ]
 
-{
-    "user_id":"1",
-    "post_id":"1",
-}
+
